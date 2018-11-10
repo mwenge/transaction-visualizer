ac36b972abb971448a1bc0943b0f8d8b2e9b1b23
Robert Hogan <robhogan@gmail.com>
2018-11-10 10:32:24 +0000

    Initial version of map

--- 020 - Simple Web Server.go
+++ 020 - Simple Web Server.go
@@ -1,9 +1,29 @@
 package main
 import (
+  "bufio"
+  "io"
+  "log"
   "net/http"
+  "os"
+  "time"
 )
 
 func main() {
+  helloHandler := func(w http.ResponseWriter, req *http.Request) {
+    file, err := os.Open("./900-Geo.txt")
+    if err != nil {
+      log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {             // internally, it advances token based on sperator
+      io.WriteString(w, scanner.Text() + "\r\n")  // token in unicode-char
+      time.Sleep(10 * time.Millisecond)
+    }
+  }
+
+  http.HandleFunc("/900-Geo.txt", helloHandler)
   http.Handle("/", http.FileServer(http.Dir("./")))
   if err := http.ListenAndServe(":8080", nil); err != nil {
     panic(err)
